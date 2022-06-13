6d275f68fa5d5f72703a36f6cf604a76d35e7d54
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-13 21:04:32 +0300

    Add task003

---
+++ task003/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 2D пространстве.
+
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = Input("Введите координату х1: ");
+int y1 = Input("Введите координату y1: ");
+int x2 = Input("Введите координату х2: ");
+int y2 = Input("Введите координату y2: ");
+
+int lineX = x2 - x1;
+int lineY = y2 - y1;
+
+Console.WriteLine(Math.Sqrt((lineX*lineX) + lineY*lineY));
+
+
