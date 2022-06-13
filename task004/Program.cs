df9ab16233b2057fc8b9d60169dfe9b968b22bad
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-13 21:28:31 +0300

    Add task004

---
+++ task004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу квадратов чисел от 1 до N.
+
+int Square (int a) // метод для нахождения квадрата
+{
+    return a * a;
+}
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count < n)
+{
+    int sq = Square(count);
+    count ++;
+    Console.Write($"{sq}, ");
+}
+Console.WriteLine(Square(n));
