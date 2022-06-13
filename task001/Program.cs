7331315a743733bfec0a7dc87e0aa9ecde347247
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-13 20:21:22 +0300

    Add task 001

---
+++ task001/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+int x = Input("Введите координату x: ");
+int y = Input("Введите координату y: ");
+
+if(x == 0 || y == 0)
+{
+    Console.WriteLine("x и y не могут быть равны 0");
+    return;
+}
+
+int result = GetNumberQuarter(x, y);
+Console.WriteLine($"{result} четверть");
+
+int GetNumberQuarter(int x, int y)
+{
+    int result = 0;
+    if(x > 0 && y > 0)
+    {
+        result = 1;
+    }
+    if(x < 0 && y > 0)
+    {
+        result = 2;
+    }
+    if(x < 0 && y < 0)
+    {
+        result = 3;
+    }
+    if(x > 0 && y < 0)
+    {
+        result = 4;
+    }
+    return result;
+}
+
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
