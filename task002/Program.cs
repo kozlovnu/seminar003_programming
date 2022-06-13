d3124044ec8207a04155f2daeef04dbb88a74ea2
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-13 20:41:34 +0300

    Add task002

---
+++ task002/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру 
+// четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int n = Input("Номер четверти: ");
+
+while (n >= 1 && n <= 4)
+{
+
+    if (n == 1)
+
+    {
+        Console.WriteLine(" x > 0, y > 0");
+    }
+
+    else if (n == 2)
+    {
+        Console.WriteLine(" x < 0, y > 0");
+    }
+    else if (n == 3)
+    {
+        Console.WriteLine(" x < 0, y < 0");
+    }
+    else if (n == 4)
+    {
+        Console.WriteLine(" x > 0, y < 0");
+    }
+    break;
+}
